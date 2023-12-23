af7c17eb29db1f62fda9f71ca7acc1ac50da4870
Abdelaziz Mahdy <zezohassam@gmail.com>
2023-12-23 22:41:15 +0200

    fix android compilation and scalling

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
     return OKToast(
       position: ToastPosition.bottom,
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
           title: 'Flutter Demo',
           theme: ThemeData(
             // This is the theme of your application.
